62570ce7606522268c11a8511573f513b8b9eca6
chypearlnel <38863834+chypearlnel@users.noreply.github.com>
2018-04-30 17:53:15 +0100

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+	<title>Figma design</title>
+	<style> 
+	.container{
+		height: 650;
+	}
+	.head{
+		background-color: #DAEFEF;
+		height: 200;
+		margin-bottom: -3%;
+	}
+
+	.body{background-color: #E0E0E0;
+		height:450;
+		padding-left: 10px;
+		padding-top: 10px;
+		
+		
+		
+	}
+
+	h1{
+		font-family: Roboto;
+		font-size: 65;
+		text-align:center;
+		color: #219653;
+		font-weight: 50;
+	}
+	h4{
+		font-family: Roboto;
+		font-size: 36;
+		text-align:center;
+		color: #000000;
+		font-weight: 50;
+		margin-top: -10px;
+
+	}
+	h3{
+		font-family: Roboto;
+		color: #219653;
+		text-align: left;
+		font-size: 38px;
+	}
+
+	em {
+		font-family: Roboto;
+		color: #333333;
+		font-size: 36;
+		float: right;
+		padding-top: 200px;
+	}
+
+	</style>
+</head>
+
+<body>
+	<div class = "container">
+		<div class="head"> 
+			<h1>HNG INTERNSHIP 4.0</h1>
+		 	<h4> @slack/hnginternship4 </h4>
+		</div>
+		<div class="body"> 
+			<h3><?php echo "Date:". date("m,Y"); ?> </h3>
+			<h3><?php echo "Time:". date("h:ia"); ?> </h3>
+			
+			
+			<h5><em> Proudly an intern! </em><h5>
+
+		</div>
+	</div>
+
+</body>
+
+</html>
